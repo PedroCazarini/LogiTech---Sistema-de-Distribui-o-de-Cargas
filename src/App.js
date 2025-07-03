2d48fad001a4f7646854c6ece35def8748d898e1
Pedro Cazarini <contato.pedrocazarinipereira@gmail.com>
2025-07-03 11:12:54 -0300

    Removido duplicação no retorno das informações de não_alocados

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import DistribuirPedidos from './components/DistribuirPedidos';
-import NaoAlocados from './components/NaoAlocados';
 
 function App() {
   return (
     <div className="App" style={{ padding: '20px' }}>
       <h1>Logitech - Distribuição de Cargas</h1>
       <DistribuirPedidos />
-      <NaoAlocados />
     </div>
   );
 }
